076f5f09b88caec43dc4e6d91bf27cd575faf6c5
Dolly <tarintaj1027@gmail.com>
2026-07-04 23:04:15 +0530

    Add embedding generation using Sentence Transformer

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from utils.pdf_reader import load_pdf
 from utils.text_splitter import split_text
+from utils.embeddings import generate_embeddings
 
 text = load_pdf("data/machine_learning.pdf")
 
 chunks = split_text(text)
 
+embeddings = generate_embeddings(chunks)
+
 print(f"Total Chunks: {len(chunks)}")
+print(f"Total Embeddings: {len(embeddings)}")
 
 print("\nFirst Chunk:\n")
 print(chunks[0])
+
+print("\nEmbedding Dimension:")
+print(len(embeddings[0]))
